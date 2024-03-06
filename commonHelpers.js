81bfcf2bb7d329dde2145a845f03cd066ca94b92
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-03-06 12:22:29 +0000

    Deploying to gh-pages from @ Alexandr-1973/goit-js-hw-12@64703f6a5d8ad021ff684505f1b79d2944cd70d0 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,14 @@
-import{i as f,a as d}from"./assets/vendor-8cce9181.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const r of i.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function t(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(e){if(e.ep)return;e.ep=!0;const i=t(e);fetch(e.href,i)}})();const u=document.querySelector(".shopping-list-title");function l(){return JSON.parse(localStorage.getItem("addBook"))}function g(s){h();const o=l().map(t=>`<li class="shoppinglist-book" id="${t._id}">
+import{i as f,a as d}from"./assets/vendor-8cce9181.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const r of i.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function e(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerPolicy&&(i.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?i.credentials="include":t.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(t){if(t.ep)return;t.ep=!0;const i=e(t);fetch(t.href,i)}})();const u=document.querySelector(".shopping-list-title");function l(){return JSON.parse(localStorage.getItem("addBook"))}function g(s){h();const o=l().map(e=>`<li class="shoppinglist-book" id="${e._id}">
           <img
             class="shoppinglist-book-image"
-            src="${t.book_image}"
+            src="${e.book_image}"
             alt="book image"
           />
           <div class="shoppinglist-total-div">
             <div class="shoppinglist-tb-div">
               <div class="shoppinglist-titcat-div">
-                <h3 class="shoppinglist-book-title">${t.title}</h3>
-                <p class="shoppinglist-book-category">${t.list_name}</p>
+                <h3 class="shoppinglist-book-title">${e.title}</h3>
+                <p class="shoppinglist-book-category">${e.list_name}</p>
               </div>
 
               <button
@@ -18,15 +18,15 @@ import{i as f,a as d}from"./assets/vendor-8cce9181.js";(function(){const o=docum
             </div>
 
             <div class="shoppinglist-desauthorlink">
-              <p class="shoppinglist-book-description">${t.description}</p>
+              <p class="shoppinglist-book-description">${e.description}</p>
 
               <div class="shoppinglist-low-div">
-                <p class="shoppinglist-book-author">${t.author}</p>
+                <p class="shoppinglist-book-author">${e.author}</p>
                 <ul class="shoppinglist-book-low-ul">
                   <li class="shoppinglist-book-low-li-amazon">
                     <a
                       class="shopping-amazon-link"
-                      href="${t.amazon_product_url}"
+                      href="${e.amazon_product_url}"
                       target="_blank"
                     >
                       <img
@@ -39,7 +39,7 @@ import{i as f,a as d}from"./assets/vendor-8cce9181.js";(function(){const o=docum
                   <li class="shoppinglist-book-low-li-apple">
                     <a
                       class="shopping-apple-link"
-                      href="${t.buy_links[1].url}"
+                      href="${e.buy_links[1].url}"
                       target="_blank"
                     >
                       <img
@@ -60,5 +60,5 @@ import{i as f,a as d}from"./assets/vendor-8cce9181.js";(function(){const o=docum
                 class="shopping-list-img"
                 src="./img/books.png"
                 alt="a lot of books"
-              />`;u.insertAdjacentHTML("afterend",s),f.info({message:"The list of books is empty.",timeout:3e3,position:"topRight"})}function h(){document.querySelector(".shopping-list-img")&&(document.querySelector(".shopping-list-img").remove(),document.querySelector(".shopping-list-text").remove())}document.querySelector(".shopping-list").addEventListener("click",s=>{if(s.preventDefault(),s.target.nodeName=="BUTTON"){const o=s.target.closest("li").getAttribute("id");console.log(o);const t=l().filter(e=>e._id!==o);S(t),document.querySelector(`li[id="${o}"]`).remove()}l().length===0&&m()});l()&&l().length>0?g(l()):m();let a=[],c=[];async function b(){c=(await d.get("https://books-backend.p.goit.global/books/top-books")).data;for(let o=0;o<=5;o+=1)for(let t=0;t<=4;t+=1)a.push(c[o].books[t]._id)}b();let p=[];async function k(){for(let s=0;s<a.length;s+=4){const o=await d.get(`https://books-backend.p.goit.global/books/${a[s]}`);p.push(o.data)}localStorage.setItem("addBook",JSON.stringify(p)),g(l())}document.querySelector(".techbutton").addEventListener("click",y);function y(){k()}document.querySelector(".techbuttondel").addEventListener("click",v);function v(){localStorage.removeItem("addBook")}JSON.parse(localStorage.getItem("addBook"));function S(s){localStorage.setItem("addBook",JSON.stringify(s))}
+              />`;u.insertAdjacentHTML("afterend",s),f.info({message:"The list of books is empty.",timeout:3e3,position:"topRight"})}function h(){document.querySelector(".shopping-list-img")&&(document.querySelector(".shopping-list-img").remove(),document.querySelector(".shopping-list-text").remove())}document.querySelector(".shopping-list").addEventListener("click",s=>{if(s.preventDefault(),s.target.nodeName=="BUTTON"){const o=s.target.closest("li").getAttribute("id");console.log(o);const e=l().filter(t=>t._id!==o);S(e),document.querySelector(`li[id="${o}"]`).remove()}l().length===0&&m()});l()&&l().length>0?g(l()):m();let a=[],c=[];async function b(){c=(await d.get("https://books-backend.p.goit.global/books/top-books")).data;for(let o=0;o<=5;o+=1)for(let e=0;e<=4;e+=1)a.push(c[o].books[e]._id)}b();let p=[];async function k(){for(let s=0;s<a.length;s+=4){const o=await d.get(`https://books-backend.p.goit.global/books/${a[s]}`);p.push(o.data)}localStorage.setItem("addBook",JSON.stringify(p)),g(l())}document.querySelector(".techbutton").addEventListener("click",y);function y(){k()}JSON.parse(localStorage.getItem("addBook"));function S(s){localStorage.setItem("addBook",JSON.stringify(s))}
 //# sourceMappingURL=commonHelpers.js.map
